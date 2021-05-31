27efd4d06b902316a9eb41883d90fe0979b8a0dd
Adhyfa Fahmy Hidayat <adhyfafahmy@gmail.com>
2021-05-31 15:20:30 +0700

    commit all

---
+++ App/Pods/Target Support Files/Pods-SetupPin/Pods-SetupPin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SetupPin : NSObject
+@end
+@implementation PodsDummy_Pods_SetupPin
+@end
---
+++ App/Pods/Target Support Files/Pods-TransferDetail/Pods-TransferDetail-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TransferDetail : NSObject
+@end
+@implementation PodsDummy_Pods_TransferDetail
+@end
